c047efd58981cda810af5afade10bc372c0ae3ad
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-06 12:19:32 -0500

    add grid and content to Pricing page

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -13,9 +13,48 @@ export default function Pricing() {
           </h2>
         </div>
         <div class="row options">
-          <div class="col-sm">One of three columns</div>
-          <div class="col-sm">One of three columns</div>
-          <div class="col-sm">One of three columns</div>
+          <div class="col-lg plan">
+            <h1>BASIC</h1>
+            ____________
+            <h1>MONTHLY</h1>
+            <h1>$9</h1>
+            <ul className="repeating-counter-rule">
+              <li>♪ Very Good</li>
+              <li>♪ Amazing</li>
+              <li>♪ Perfect Job</li>
+              <li>♪ Love this</li>
+              <li>♪ It's so good</li>
+              <li>♪ Features</li>
+            </ul>
+          </div>
+          <div class="col-lg plan">
+            <h1>ADVANCED</h1>
+            ____________
+            <h1>YEARLY</h1>
+            <h1>$99</h1>
+            <ul>
+              <li>♪ Very very Good</li>
+              <li>♪ Even Amazing</li>
+              <li>♪ Perfect job</li>
+              <li>♪ Love this more</li>
+              <li>♪ It's so so good</li>
+              <li>♪ More Features</li>
+            </ul>
+          </div>
+          <div class="col-lg plan">
+            <h1>PRO</h1>
+            ____________
+            <h1>YEARLY</h1>
+            <h1>$120</h1>
+            <ul>
+              <li>♪ Very very Good</li>
+              <li>♪ Even more</li>
+              <li>♪ Perfect job</li>
+              <li>♪ Love this more</li>
+              <li>♪ It's so so good</li>
+              <li>♪ More Features</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
