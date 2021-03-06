4920a73a18b608036d407cd14fe4c75ff6968fca
ThomasA64 <thomas.arthur64@gmail.com>
2021-03-06 14:18:57 -0500

    add colour to different pricing options

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,5 +2,5 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 
 export default function Nav() {
-  return <nav class="navstyle">HAMBURGER</nav>;
+  return <nav class="navstyle">||| EXP|CON</nav>;
 }
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -13,12 +13,12 @@ export default function Pricing() {
           </h2>
         </div>
         <div class="row options">
-          <div class="col-lg plan">
+          <div class="col-lg plan basic">
             <h1>BASIC</h1>
             ____________
             <h1>MONTHLY</h1>
-            <h1>$9</h1>
-            <ul className="repeating-counter-rule">
+            <h1 className="price">$9</h1>
+            <ul className="features">
               <li>♪ Very Good</li>
               <li>♪ Amazing</li>
               <li>♪ Perfect Job</li>
@@ -26,13 +26,19 @@ export default function Pricing() {
               <li>♪ It's so good</li>
               <li>♪ Features</li>
             </ul>
+            <button class="sel bas">
+              {" "}
+              <a className="features sela" href="/pricing">
+                SELECT
+              </a>
+            </button>
           </div>
-          <div class="col-lg plan">
+          <div class="col-lg plan advanced">
             <h1>ADVANCED</h1>
             ____________
             <h1>YEARLY</h1>
-            <h1>$99</h1>
-            <ul>
+            <h1 className="price">$99</h1>
+            <ul className="features">
               <li>♪ Very very Good</li>
               <li>♪ Even Amazing</li>
               <li>♪ Perfect job</li>
@@ -40,13 +46,19 @@ export default function Pricing() {
               <li>♪ It's so so good</li>
               <li>♪ More Features</li>
             </ul>
+            <button class="sel adv">
+              {" "}
+              <a className="features sela" href="/pricing">
+                SELECT
+              </a>
+            </button>
           </div>
-          <div class="col-lg plan">
+          <div class="col-lg plan pro">
             <h1>PRO</h1>
             ____________
             <h1>YEARLY</h1>
-            <h1>$120</h1>
-            <ul>
+            <h1 className="price">$120</h1>
+            <ul className="features">
               <li>♪ Very very Good</li>
               <li>♪ Even more</li>
               <li>♪ Perfect job</li>
@@ -54,6 +66,12 @@ export default function Pricing() {
               <li>♪ It's so so good</li>
               <li>♪ More Features</li>
             </ul>
+            <button class="sel prosel">
+              {" "}
+              <a className="features sela" href="/pricing">
+                SELECT
+              </a>
+            </button>
           </div>
         </div>
       </div>
